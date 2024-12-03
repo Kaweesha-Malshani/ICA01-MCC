e5a7771f9a214585d4894085225c4dc7fb9e9933
Kaweesha-Malshani <537tharu@gmail.com>
2024-12-03 22:02:00 +0530

    edit product.js

--- myapp/src/components/Product.js
+++ myapp/src/components/Product.js
@@ -1,18 +1,28 @@
 import '../assets/CSS/layout.css';
-export default function Product(){
+import React, { useState } from 'react';
+export default function Product({ flower, addToCart}){
     
+    const [quantity, setQuantity] = useState(0);
+
+    const handleAddToCart = () => {
+        if (quantity > 0) {
+            addToCart(flower, quantity);
+            setQuantity(0); 
+        }
+    };
+
     return(
         <div className="grid-item">
 
             <div class="card">
-                <img  />
+                 <img src={require('../assets/image/'+flower.img)} alt={flower.name} />
                 <div class="card-body">
                     <h5 class="card-title">Price:</h5>
                     <div class="quantity-container">
                         <label for="quantity">Quantity:</label>
                         <input type="number" id="quantity" name="quantity" />
                     </div>
-                    <button class="card-button">Add to Cart</button>
+                    <button class="card-button" onClick={handleAddToCart}>Add to Cart</button>
                 </div>
             </div>
         </div>
